9a34b3822846c4543adc1ecc49e8c2f1c33f7dd2
Mamdouh Khaled <Mamdouh.khaled@live.com>
2021-11-14 17:59:00 +0200

    Refactor Route And Add More Routes

--- App/Route/web.php
+++ App/Route/web.php
@@ -1,9 +1,16 @@
 <?php
-use Core\Route;
+use \App\Core\Route;
+Route::get('users', [App\App\Controllers\UserController::class, 'index'] );
+Route::get('users/show', [App\App\Controllers\UserController::class, 'show'] );
+Route::post('users/store', [App\App\Controllers\UserController::class, 'store'] );
+Route::get('users/update', [App\App\Controllers\UserController::class, 'update'] );
+Route::get('users/delete', [App\App\Controllers\UserController::class, 'delete'] );
 
-Route::get('users', [\App\Controllers\UserController::class, 'index'] );
-Route::get('users/show', [\App\Controllers\UserController::class, 'show'] );
-Route::post('users/store', [\App\Controllers\UserController::class, 'store'] );
-Route::get('users/update', [\App\Controllers\UserController::class, 'update'] );
-Route::get('users/delete', [\App\Controllers\UserController::class, 'delete'] );
 
+
+
+Route::post('register', [App\App\Controllers\RegisterController::class, 'store'] );
+Route::get('/', [App\App\Controllers\RegisterController::class, 'show'] );
+Route::get('register', [App\App\Controllers\RegisterController::class, 'create'] );
+
+Route::resolve();
--- Core/Route.php
+++ Core/Route.php
@@ -1,22 +1,31 @@
 <?php
-namespace Core;
+namespace App\Core;
 
 class Route {
     private $routes = [];
     protected static array $instances;
-
+    protected string $Home = 'HOME';
 
 
     protected function get(array $route) {
+        if($route[0] == '/'){
+            $route[0] = $this->Home;
+        }
         $this->routes['get'][ltrim($route[0], '/')] = $route[1];
     }
     protected function post(array $route) {
+        if($route[0] == '/'){
+            $route[0] = $this->Home;
+        }
         $this->routes['post'][ltrim($route[0], '/')] = $route[1];
     }
 
     protected function resolve()
     {
         $controller = ltrim($_SERVER['REQUEST_URI'], '/');
+        if(empty( $controller) ){
+            $controller = $this->Home;
+        }
         $method = strtolower(ltrim($_SERVER['REQUEST_METHOD']));
         if (isset($this->routes[$method][$controller])){
             $class = $this->routes[$method][$controller];
@@ -28,17 +37,13 @@ class Route {
 
     public static function __callStatic($method, $args)
     {
+
         $cls = static::class;
         if (!isset(self::$instances[$cls])) {
             self::$instances[$cls] = new static();
         }
         return self::$instances[$cls]->{$method}($args);
-    }
 
-    public function __destruct()
-    {
-        $this->resolve();
     }
-
 
 }
