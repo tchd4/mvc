204dcfd54c86e0752f0115677bdcebb462ab61e2
Mamdouh Khaled <Mamdouh.khaled@live.com>
2021-11-14 17:55:57 +0200

    Clean index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
-require __DIR__.'/../Core/Autoloader.php';
+ob_start();
+
+
+
+
+require dirname(__DIR__).'/Core/Exception.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
+\App\Core\Session::start();
 require __DIR__.'/../Core/Helper.php';
 require __DIR__.'/../App/Route/web.php';
 
